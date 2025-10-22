e0d705ef2534861d13a940e8bcb25f2e22331b91
SashaGiT <sashaotero5@gmail.com>
2025-10-22 18:34:47 +0000

    Segundo commit

--- src/app.py
+++ src/app.py
@@ -3,10 +3,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask_jwt_extended import create_access_token, jwt_required
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import User, db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
@@ -59,6 +60,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -66,6 +69,36 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    if 'email' not in body:
+        return jsonify({"msg": "Missing email parameter"}), 400
+    if 'password' not in body:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    user = User.query.filter_by(email=body['email']).first()
+    print(user)
+    if user is None:
+        return jsonify({"msg": "User not found"}), 400
+    if user.password != body['password']:
+        return jsonify({"msg": "Bad password"}), 400
+    access_token = create_access_token(identity=user.email)
+
+    return jsonify({'msg': 'Login successful',
+                    'token': access_token}), 200
+
+
+@app.route('/private', methods=['GET'])
+@jwt_required()
+def privado():
+    return jsonify({"msg": "This is a private route"}), 200
+
+    # Here you would typically verify the email and password with the database
+    # For demonstration, let's assume a user with email '
 
 
 # this only runs if `$ python src/main.py` is executed
