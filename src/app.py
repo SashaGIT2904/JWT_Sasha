298e8955fbb9aaebea5e9f820b197d06364d20d3
SashaGiT <sashaotero5@gmail.com>
2025-10-27 17:06:22 +0000

    JWT Funciona

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
@@ -12,12 +12,16 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
+from flask_bcrypt import Bcrypt
+
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 app.url_map.strict_slashes = False
 
 app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')
@@ -70,8 +74,8 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
-
-@app.route('/login', methods=['POST'])
+#LOGIN (funciona)
+@app.route('/api/login', methods=['POST'])
 def login():
     body = request.get_json(silent=True)
     if body is None:
@@ -81,32 +85,46 @@ def login():
     if 'password' not in body:
         return jsonify({"msg": "Missing password parameter"}), 400
     user = User.query.filter_by(email=body['email']).first()
-    print(user)
     if user is None:
-        return jsonify({"msg": "User not found"}), 400
-    if user.password != body['password']:
-        return jsonify({"msg": "Bad password"}), 400
+        return jsonify({"msg": "User not found"}), 404
+    if not bcrypt.check_password_hash(user.password, body['password']):
+        return jsonify({"msg": "Bad password"}), 401
     access_token = create_access_token(identity=user.email)
+    return jsonify({'msg': 'Login successful', 'token': access_token}), 200
 
-    return jsonify({'msg': 'Login successful',
-                    'token': access_token}), 200
-
-
-@app.route('/private', methods=['GET'])
+# PRIVADA (funciona)
+@app.route('/api/private', methods=['GET'])
 @jwt_required()
 def privado():
     current_user = get_jwt_identity()
     user = User.query.filter_by(email=current_user).first()
     return jsonify({"msg": "This is a private route"}), 200
 
-    # Here you would typically verify the email and password with the database
-    # For demonstration, let's assume a user with email '
+# Crear el Token de registro, cerrar sesión(limpiar el local storage), ruta privada
+
+#REGISTER (funciona)
+@app.route('/api/register', methods=['POST'])
+def register():
+    body = request.get_json()
+    user = User()
+    user.email = body['email']
+    hash_password = bcrypt.generate_password_hash(
+        body['password']).decode('utf-8')
+    user.password = hash_password
+    user.is_active = True
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"msg": "User created"}), 200
+
+
+@app.route('/api/logout', methods=['POST'])
+def logout():
+    return jsonify({"msg": "Logout successful"}), 200
+
+# Bcrypt
 
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
-
-
-#Crear el Token de registro, cerrar sesión(limpiar el local storage), ruta privada
